2f609a63e8ce4da8c930704141c16a35843d7ab5
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-26 17:55:59 +0300

    del brench

--- SeptemberFitness.BL/Controller/UserControllers.cs
+++ SeptemberFitness.BL/Controller/UserControllers.cs
@@ -17,37 +17,22 @@ namespace SeptemberFitness.BL.Controller
         /// <summary>
         /// Пользователь приложения.
         /// </summary>
-        public User User { get; }
+        public User CurrentUser { get; }
+
+        public List<User> Users { get; }
 
         /// <summary>
         /// Создать контроллер нового пользователя.
         /// </summary>
         /// <param name="user"></param>
-        public UserControllers(string userName, string genderName, DateTime birthdate, double weight, double height)
+        public UserControllers(string name)
         {
-            //конструктор упаковывает класс User 
-            Gender gender = new Gender(genderName);
-            User = new User(userName, gender, birthdate, weight, height);
-           
-        }
-        /// <summary>
-        /// Получить данные пользователя.
-        /// </summary>
-        /// <returns></returns>
-        /// 
-        public UserControllers()
-        {
-            var formatter = new BinaryFormatter();
-
-            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            if(name == null)
             {
-                if (formatter.Deserialize(fs) is User user)
-                {
-                    User = user;
-
-                }
-                // TODO : Что делать если не считали пользователя.
+                throw new ArgumentNullException("Юзер не может быть Null", nameof(name));
             }
+
+            
         }
         /// <summary>
         /// Сохранить данные пользователя.
@@ -58,10 +43,26 @@ namespace SeptemberFitness.BL.Controller
 
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                formatter.Serialize(fs, User); //serialize 
+                formatter.Serialize(fs, Users); //serialize 
             }
         }
+        /// <summary>
+        /// Получить данные пользователя.
+        /// </summary>
+        /// <returns></returns>
+        public List<User> Load()
+        {
+            var formatter = new BinaryFormatter();
 
-        
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (formatter.Deserialize(fs) is List<User> Users)
+                {
+                    return Users;
+
+                }
+                    return null;
+            }
+        }
     }
 }
