a829f4d2c0ba4634479784867381f54850e7b4f3
septemberEatMe <vitaliiseptember@gmail.com>
2019-10-01 15:21:28 +0300

    add Food and Eating

---
+++ SeptemberFitness.BL/Model/Eating.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptemberFitness.BL.Model
+{
+    /// <summary>
+    /// Приём пищи.
+    /// </summary>
+    class Eating
+    {
+        /// <summary>
+        /// Время приёма.
+        /// </summary>
+        public DateTime Moment{ get;}
+        /// <summary>
+        /// Список еды.
+        /// </summary>
+        public Dictionary <Food, double> Foods { get;}
+
+        public User User { get; }
+
+        public Eating(User user)
+        {
+            if (user is null)
+            {
+                throw new ArgumentNullException("Юзер не может быть пустым, ведь он ЮЗЕР!!!", nameof(user));
+            }
+            User = user;
+            Moment = DateTime.UtcNow;
+            Foods = new Dictionary<Food, double>();
+        }
+
+        public void FoodAdd(Food food , double weight)
+        {
+
+            var product = Foods.Keys.FirstOrDefault(f => f.Name.Equals(food));  //проверка на вхождение продукта
+            if(product == null)
+            {
+                Foods.Add(food, weight);
+            }
+            else
+            {
+                Foods[product] += weight;
+            }
+        }
+    }
+}
---
+++ SeptemberFitness.BL/Model/Food.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptemberFitness.BL.Model
+{
+    public class Food
+    {
+        public string Name { get; }
+        /// <summary>
+        /// Калории.
+        /// </summary>
+        public double Calories { get; }
+        /// <summary>
+        /// Белки.
+        /// </summary>
+        public double Proteins{ get; }
+        /// <summary>
+        /// Жиры.
+        /// </summary>
+        public double Fats { get; }
+        /// <summary>
+        /// Углеводы.
+        /// </summary>
+        public double Carbohydrates { get; }
+
+    
+
+        public Food(string name) : this (name, 0, 0, 0, 0)
+        {
+            //TODO: проверка
+        }
+
+        public Food(string name, double calories ,double proteins, double fats, double carbohydrates)
+        {
+            //TODO : Проверка.
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Имя не может быть пустым или Null", nameof(name));
+            }
+
+
+            Name = name;
+            Calories = calories / 100.0;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+        }
+
+        public override string ToString() => Name;
+           
+     
+    }
+}
